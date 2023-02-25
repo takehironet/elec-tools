9ab6e8662b74179c0c10f87a0111a9e716926b05
Takehiro Matsushima <takehiro.dreamizm@gmail.com>
2023-02-26 01:37:23 +0900

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <h1>takehironet.github.io</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+</main>
+
+<style>
+
+</style>
